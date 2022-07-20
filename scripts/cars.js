e789c0db530cf7ca2870769acb88b7ca3d5c6f00
Aman Vyas <aman.vyas.88864@gmail.com>
2022-07-21 01:46:38 +0530

    cars page

---
+++ scripts/cars.js
@@ -0,0 +1,332 @@
+let carData = [{name:'Maruti Suzuki Celerio',image1:'https://apollo-singapore.akamaized.net/v1/files/w9ec24e93xdr2-IN/image;s=780x0;q=60',image2:'https://apollo-singapore.akamaized.net/v1/files/03v2ayjxgu0o1-IN/image;s=780x0;q=60',image3:'https://apollo-singapore.akamaized.net/v1/files/jq9c5654hbja2-IN/image;s=780x0;q=60',price:370000,km:60000,brand:'Maruti Suzuki',address:'Kamod Nagar',city:'Mumbai',description:`ADDITIONAL VEHICLE INFORMATION:
+ABS: Yes
+Adjustable External Mirror: Manual
+Adjustable Steering: Yes
+Air Conditioning: Automatic Climate Control
+Number of Airbags: None
+Anti Theft Device: Yes
+Aux Compatibility: Yes
+Color: Silver
+Engine Capacity/Displacement (in Cc): 1200
+Insurance Type: Comprehensive
+Lock System: Remote Controlled Central
+Make Month: May
+Power steering: Yes
+Power Windows: Front & rear
+AM/FM Radio: Yes
+Registration Place: MH
+Finance: Yes
+USB Compatibility: Yes`},
+{name:'Hyundai Creta',image1:'https://apollo-singapore.akamaized.net/v1/files/aj31zmylmdrx-IN/image;s=780x0;q=60',image2:'https://apollo-singapore.akamaized.net/v1/files/o9qoppqfrvxl-IN/image;s=780x0;q=60',image3:'https://apollo-singapore.akamaized.net/v1/files/gp5vam034i7o1-IN/image;s=780x0;q=60',price:800000,km:84000,brand:'Hyundai',address:'Millat Nagar',city:'Pune',description:`creta 2016
+1.6 sx key start
+second owner
+ins valid
+84000kns
+tyres 50percent
+engine very tight
+suspension good
+ac child
+location cars club Bhiwandi
+ADDITIONAL VEHICLE INFORMATION:
+ABS: Yes
+Accidental: No
+Adjustable External Mirror: Power
+Adjustable Steering: Yes
+Air Conditioning: Automatic Climate Control
+Number of Airbags: 4 airbags
+Alloy Wheels: Yes
+Anti Theft Device: Yes
+Aux Compatibility: Yes
+Battery Condition: New
+Bluetooth: Yes
+Vehicle Certified: Yes
+Color: Red
+Cruise Control: Yes
+Engine Capacity/Displacement (in Cc): 1600
+Insurance Type: Third Party
+Lock System: Remote Controlled Central
+Make Month: July
+Navigation System: Yes
+Parking Sensors: Yes
+Power steering: Yes
+Power Windows: Front & rear
+AM/FM Radio: Yes
+Rear Parking Camera: Yes
+Registration Place: MH
+Exchange: Yes
+Finance: Yes
+Service History: Available
+Tyre Condition: New
+USB Compatibility: Yes`},
+{name:'Honda City',image1:'https://apollo-singapore.akamaized.net/v1/files/r2sjf724mksu-IN/image;s=780x0;q=60',image2:'https://apollo-singapore.akamaized.net/v1/files/d8lehlj34fc13-IN/image;s=780x0;q=60',image3:'https://apollo-singapore.akamaized.net/v1/files/v4ngbq8mbnxn1-IN/image;s=780x0;q=60',price:1080000,km:19580,brand:'Honda',address:'BTM layout',city:'Bengalore',description:`Single Owner Good condition 160 Check points inspection Showroom maintained with service history Non Accidental Vehicle Non Meter Tamper No Structural Damage Finance and exchange available Warranty and festive offers
+ADDITIONAL VEHICLE INFORMATION:
+Color: Brown
+Insurance Type: Comprehensive
+Registration Place: KA
+Exchange: Yes
+Finance: Yes`},
+{name:'Ford Endeavour',image1:'https://apollo-singapore.akamaized.net/v1/files/7ueiqjxluq4w1-IN/image;s=780x0;q=60',image2:'https://apollo-singapore.akamaized.net/v1/files/fdkhhpxxkmis-IN/image;s=780x0;q=60',image3:'https://apollo-singapore.akamaized.net/v1/files/cx2tq788rpgo3-IN/image;s=780x0;q=60',price:3275000,km:56880,brand:'Ford',address:'Up hill',city:'Indore',description:`KL REGISTRATION
+WELL MAINTAINED
+DESIGN FEATURES
+Front fog lamp
+Turn indicators
+LED tail lamp
+3RD row seat 50:50 flat fold
+8 Power adjustable driver seat
+Leather wrapped gear knob & steering wheel
+SAFETY FEATURES
+ABS+EBD
+Electronic stability program
+Traction control system
+Hill launch assist
+Hill descent control
+Side airbags
+Curtain airbags
+Driver knee airbag
+COMFORT AND CONVENIENCE FEATURES
+Power foldable & adjustable outside mirrors
+Dual zone AC
+Automatic climate control
+Rear -view camera with display on center screen
+Power tailgate with anti-pinch sensor
+Rain-sensing wipers
+Cruise control
+Automatic headlamp
+Front parking sensors
+Tyre pressure monitoring system
+Heated side mirror
+Semi-auto parallel park assist
+Power folding 3rd row seat
+Electric panoramic sunroof Terrain management system
+ADDITIONAL VEHICLE INFORMATION:
+ABS: Yes
+Accidental: No
+Adjustable External Mirror: Power
+Adjustable Steering: Yes
+Air Conditioning: With Heater
+Number of Airbags: 2 airbags
+Alloy Wheels: Yes
+Anti Theft Device: Yes
+Aux Compatibility: Yes
+Battery Condition: New
+Bluetooth: Yes
+Vehicle Certified: Yes
+Color: White
+Cruise Control: Yes
+Engine Capacity/Displacement (in Cc): 3198
+Insurance Type: Comprehensive
+Lock System: Remote Controlled Central
+Make Month: March
+Navigation System: Yes
+Parking Sensors: Yes
+Power steering: Yes
+Power Windows: Front & rear
+AM/FM Radio: Yes
+Rear Parking Camera: Yes
+Registration Place: KL
+Finance: Yes
+Service History: Available
+Sunroof: Yes
+Tyre Condition: New
+USB Compatibility: Yes`},
+{name:'Tata nano',image1:'https://apollo-singapore.akamaized.net/v1/files/oo4hxj4jdkf5-IN/image;s=780x0;q=60',image2:'https://apollo-singapore.akamaized.net/v1/files/g48lxg2bymkk1-IN/image;s=780x0;q=60',image3:'https://apollo-singapore.akamaized.net/v1/files/9owleovjkrj31-IN/image;s=780x0;q=60',price:50000,km:17000,brand:'Tata',address:'Naranpura',city:'Bhopal',description:`NANO AC POWER VIDOS 4 tayer new 2 month
+ADDITIONAL VEHICLE INFORMATION:
+Adjustable External Mirror: Manual
+Air Conditioning: With Heater
+Number of Airbags: None
+Aux Compatibility: Yes
+Battery Condition: New
+Bluetooth: Yes
+Vehicle Certified: Yes
+Color: Yellow
+Engine Capacity/Displacement (in Cc): 699
+Insurance Type: Third Party
+Lock System: Central
+Make Month: February
+Power Windows: Front
+AM/FM Radio: Yes
+Registration Place: GJ
+Service History: Available
+Tyre Condition: New
+USB Compatibility: Yes
+`},
+{name:'Maruti Suzuki Ertiga',image1:'https://apollo-singapore.akamaized.net/v1/files/kwn2fwt64kec2-IN/image;s=780x0;q=60',image2:'https://apollo-singapore.akamaized.net/v1/files/ds6nnnnz2f822-IN/image;s=780x0;q=60',image3:'https://apollo-singapore.akamaized.net/v1/files/uobeaojeq2eb2-IN/image;s=780x0;q=60',price:575000,km:114000,brand:'Maruti Suzuki',address:'perkit',city:'Delhi',description:'Good cundution'},
+{name:'Tata Harrier',image1:'https://apollo-singapore.akamaized.net/v1/files/wafddclygt3s2-IN/image;s=780x0;q=60',image2:'https://apollo-singapore.akamaized.net/v1/files/jchljsswpkp32-IN/image;s=780x0;q=60',image3:'https://apollo-singapore.akamaized.net/v1/files/5lvn3oreqbw2-IN/image;s=780x0;q=60',price:1375000,km:38000,brand:'Tata',address:'Kapodra',city:'Mumbai',description:`ADDITIONAL VEHICLE INFORMATION:
+Color: White
+Insurance Type: Third Party
+Make Month: October
+Registration Place: GJ`},
+{name:'Hyundai I20',image1:'https://apollo-singapore.akamaized.net/v1/files/a6tmmbjbc3wp3-IN/image;s=780x0;q=60',image2:'https://apollo-singapore.akamaized.net/v1/files/heljz6ekn6rs2-IN/image;s=780x0;q=60',image3:'https://apollo-singapore.akamaized.net/v1/files/3lf6zkf7w48g-IN/image;s=780x0;q=60',price:625000,km:16000,brand:'Hyundai',address:'sector 50',city:'Pune',description:`New. Condition car new tyre full insurance top model sportz auto fold mirror Sequential fitted cng
+ADDITIONAL VEHICLE INFORMATION:
+Accidental: No
+Adjustable External Mirror: Power
+Adjustable Steering: Yes
+Air Conditioning: With Heater
+Number of Airbags: 2 airbags
+Aux Compatibility: Yes
+Battery Condition: New
+Bluetooth: Yes
+Vehicle Certified: Yes
+Color: Grey
+Cruise Control: Yes
+Insurance Type: Comprehensive
+Lock System: Remote Controlled Central
+Make Month: July
+Navigation System: Yes
+Parking Sensors: Yes
+Power steering: Yes
+Power Windows: Front & rear
+AM/FM Radio: Yes
+Registration Place: DL
+Exchange: Yes
+Finance: Yes
+Service History: Available
+Tyre Condition: New
+USB Compatibility: Yes`},
+{name:'Honda Amaze',image1:'https://apollo-singapore.akamaized.net/v1/files/acwwvfnu1egm2-IN/image;s=780x0;q=60',image2:'https://apollo-singapore.akamaized.net/v1/files/mjxa2jjprsqc3-IN/image;s=780x0;q=60',image3:'https://apollo-singapore.akamaized.net/v1/files/7mcjtbm4y1gk3-IN/image;s=780x0;q=60',price:480000,km:52000,brand:'Honda',address:'saket',city:'Bengalore',description:`insurance finance warranty available
+ADDITIONAL VEHICLE INFORMATION:
+Accidental: No
+Adjustable External Mirror: Manual
+Air Conditioning: With Heater
+Number of Airbags: None
+Aux Compatibility: Yes
+Battery Condition: New
+Bluetooth: Yes
+Vehicle Certified: Yes
+Color: Silver
+Insurance Type: Comprehensive
+Lock System: Central
+Make Month: November
+Power steering: Yes
+Power Windows: Front & rear
+AM/FM Radio: Yes
+Registration Place: DL
+Exchange: Yes
+Finance: Yes
+Service History: Available
+Tyre Condition: New
+USB Compatibility: Ye`},
+{name:'Ford Figo',image1:'https://apollo-singapore.akamaized.net/v1/files/gp6ub0hmehs72-IN/image;s=780x0;q=60',image2:'https://apollo-singapore.akamaized.net/v1/files/fg8p7qgd31y71-IN/image;s=780x0;q=60',image3:'https://apollo-singapore.akamaized.net/v1/files/o1tibp94zlls3-IN/image;s=780x0;q=60',price:350000,km:20500,brand:'Ford',address:'vidiya nagar',city:'Indore',description:`we arranged bank loan for our customers
+well maintained car
+recent oil service done
+பைனான்ஸ் வசதி செய்து தரப்படும்.
+ADDITIONAL VEHICLE INFORMATION:
+ABS: Yes
+Accidental: No
+Adjustable External Mirror: Manual
+Adjustable Steering: Yes
+Air Conditioning: Automatic Climate Control
+Number of Airbags: 2 airbags
+Alloy Wheels: Yes
+Aux Compatibility: Yes
+Battery Condition: New
+Bluetooth: Yes
+Vehicle Certified: Yes
+Color: White
+Cruise Control: Yes
+Insurance Type: Comprehensive
+Lock System: Remote Controlled Central`},
+{name:'Maruti Suzuki Baleno',image1:'https://apollo-singapore.akamaized.net/v1/files/gwoych19w5j82-IN/image;s=780x0;q=60',image2:'https://apollo-singapore.akamaized.net/v1/files/gh37fgnqs8hb-IN/image;s=780x0;q=60',image3:'https://apollo-singapore.akamaized.net/v1/files/tgfrofwktoch2-IN/image;s=780x0;q=60',price:655000,km:100000,brand:'Maruti Suzuki',address:'gagan vihar',city:'Bhopal',description:`ADDITIONAL VEHICLE INFORMATION:
+Accidental: No
+Battery Condition: New
+Vehicle Certified: Yes
+Color: Blue
+Insurance Type: Comprehensive
+Exchange: Yes
+Finance: Yes
+Service History: Available
+Tyre Condition: New`},
+{name:'Tata Indica Vista',image1:'https://apollo-singapore.akamaized.net/v1/files/f1akfs95t7jv2-IN/image;s=780x0;q=60',image2:'https://apollo-singapore.akamaized.net/v1/files/y1f6egin6d8s3-IN/image;s=780x0;q=60',image3:'https://apollo-singapore.akamaized.net/v1/files/butads6g1j2b-IN/image;s=780x0;q=60',price:100000,km:50600,brand:'Tata',address:'sunet',city:'Delhi',description:`ADDITIONAL VEHICLE INFORMATION:
+Accidental: No
+Battery Condition: Old
+Color: White
+Insurance Type: No Insurance
+Registration Place: PB
+Service History: Available
+Tyre Condition: Old`},
+{name:'Hyundai I10',image1:'https://apollo-singapore.akamaized.net/v1/files/65kvo0cix8pn2-IN/image;s=780x0;q=60',image2:'https://apollo-singapore.akamaized.net/v1/files/4eez4zi4td0g1-IN/image;s=780x0;q=60',image3:'https://apollo-singapore.akamaized.net/v1/files/d5seroo54e1b-IN/image;s=780x0;q=60',price:263000,km:45000,brand:'Hyundai',address:'navi mumbai',city:'Mumbai',description:`ADDITIONAL VEHICLE INFORMATION:
+Color: Blue
+Insurance Type: Comprehensive
+Make Month: October
+Registration Place: DL`},
+{name:'Honda city',image1:'https://apollo-singapore.akamaized.net/v1/files/r0wo57zdsflx-IN/image;s=780x0;q=60',image2:'https://apollo-singapore.akamaized.net/v1/files/shai3w6zpr5y1-IN/image;s=780x0;q=60',image3:'https://apollo-singapore.akamaized.net/v1/files/0ec4bw4jlg0l3-IN/image;s=780x0;q=60',price:440000,km:62000,brand:'Honda',address:'Vikas puri',city:'Pune',description:`HONDA CITY 2013 V TOP MODEL 2013 FIRST OWNER POWER STEERING AND WINDOWS MUSIC PLAYER WITH AUDIO STEERING CONTROLS NAVIGATION AIRBAGS ABS BREAKS ALLOYS DEFFOGGER FOG LAMPS CLIMATE CONTROL AC TOUCH PLAYER DEMAND 4,40000 NEGOTIABLE FOR TEST DRIVE VISIT OUR COMPANY OUTLET OLX AUTOS CHOPRA CARS FOR BEST PRICE AND VERIFIED BEST QUALITY CARS WITH 160 CHECK POINTS TOP QUALITY CARS IN TRICITY CHANDIGARH MOHALI PANCHKULA OFFICE ADDRESS SHOWROOM NO 2,3 AND 4 SUN TOWER BUILDING BALONGI NEAR VERKA PLANT OR MAX HOSPITAL PHASE 6 MOHALI FOR ANY INFORMATION CONTACT OLX AUTOS
+ADDITIONAL VEHICLE INFORMATION:`},
+{name:'Ford Fiesta Classic',image1:'https://apollo-singapore.akamaized.net/v1/files/qhp8u2izmjuq2-IN/image;s=780x0;q=60',image2:'https://apollo-singapore.akamaized.net/v1/files/mrlc3qvqfp452-IN/image;s=780x0;q=60',image3:'https://apollo-singapore.akamaized.net/v1/files/audnaxkf8mye-IN/image;s=780x0;q=60',price:195000,km:20080,brand:'Ford',address:'Sector 19',city:'Bengalore',description:`ADDITIONAL VEHICLE INFORMATION:
+ABS: Yes
+Accidental: No
+Adjustable External Mirror: Power
+Adjustable Steering: Yes
+Air Conditioning: Automatic Climate Control
+Number of Airbags: 2 airbags
+Alloy Wheels: Yes
+Aux Compatibility: Yes
+Battery Condition: New
+Bluetooth: Yes
+Vehicle Certified: Yes
+Color: White
+Engine Capacity/Displacement (in Cc): 1400
+Insurance Type: Comprehensive
+Lock System: Remote Controlled Central
+Make Month: March
+Parking Sensors: Yes
+Power steering: Yes`},
+{name:'',image1:'',image2:'',image3:'',price:'',km:'',brand:'Maruti Suzuki',address:'',city:'Indore',description:''},
+{name:'',image1:'',image2:'',image3:'',price:'',km:'',brand:'Tata',address:'',city:'Bhopal',description:''},
+{name:'',image1:'',image2:'',image3:'',price:'',km:'',brand:'Hyundai',address:'',city:'Delhi',description:''},
+{name:'',image1:'',image2:'',image3:'',price:'',km:'',brand:'Honda',address:'',city:'Mumbai',description:''},
+{name:'',image1:'',image2:'',image3:'',price:'',km:'',brand:'Ford',address:'',city:'Pune',description:''},
+{name:'',image1:'',image2:'',image3:'',price:'',km:'',brand:'Maruti Suzuki',address:'',city:'Bengalore',description:''},
+{name:'',image1:'',image2:'',image3:'',price:'',km:'',brand:'Tata',address:'',city:'Indore',description:''},
+{name:'',image1:'',image2:'',image3:'',price:'',km:'',brand:'Hyundai',address:'',city:'Bhopal',description:''},
+{name:'',image1:'',image2:'',image3:'',price:'',km:'',brand:'Honda',address:'',city:'Delhi',description:''},
+{name:'',image1:'',image2:'',image3:'',price:'',km:'',brand:'Ford',address:'',city:'Mumbai',description:''},
+{name:'',image1:'',image2:'',image3:'',price:'',km:'',brand:'Maruti Suzuki',address:'',city:'Pune',description:''},
+{name:'',image1:'',image2:'',image3:'',price:'',km:'',brand:'Tata',address:'',city:'Bengalore',description:''},
+{name:'',image1:'',image2:'',image3:'',price:'',km:'',brand:'Hyundai',address:'',city:'Indore',description:''},
+{name:'',image1:'',image2:'',image3:'',price:'',km:'',brand:'Honda',address:'',city:'Bhopal',description:''},
+{name:'',image1:'',image2:'',image3:'',price:'',km:'',brand:'Ford',address:'',city:'Delhi',description:''}
+]
+
+localStorage.setItem('cars', JSON.stringify(carData));
+
+let append = (data)=>{
+    let container = document.querySelector("#products")
+    container.innerHTML = null;
+    data.forEach((el)=>{
+        let card = document.createElement("div")
+        card.setAttribute("class", "items")
+        card.addEventListener("click", (event)=>{
+            addtoLS(el)
+        })
+        let imgDiv = document.createElement("div")
+        let img = document.createElement("img")
+        img.src = el.image1
+        imgDiv.append(img)
+        let detailsDiv = document.createElement("div")
+        let price = document.createElement("h3")
+        price.innerText = `₹${el.price}`
+        let km = document.createElement("p")
+        km.innerText = el.km
+        let name = document.createElement("h4")
+        name.innerText = el.name
+        let add = document.createElement("p")
+        add.innerText = el.address
+        detailsDiv.append(price,km,name,add)
+        card.append(imgDiv,detailsDiv)
+        container.append(card)
+    })
+}
+
+append(carData)
+
+let addtoLS = (el) =>{
+    localStorage.setItem("item", JSON.stringify(el))
+}
+
+
+let btt = document.querySelector("#btn-btt")
+document.querySelector("#btn-btt").addEventListener("click", () =>{
+    document.body.scrollIntoView({
+        behavior: "smooth",
+      });
+});
