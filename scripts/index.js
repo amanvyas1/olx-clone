c6bd82965c99613b8839d3856c23b9e92f75fe12
abhishek11125 <103635309+abhishek11125@users.noreply.github.com>
2022-07-21 17:30:12 +0530

    adding data of day2

---
+++ scripts/index.js
@@ -0,0 +1,114 @@
+let products = [
+    {
+        name:"Maruti Suzuki Alto K10 (VXI AMT)",
+        image1:"https://apollo-singapore.akamaized.net/v1/files/qypbum9gsjbu2-IN/image;s=780x0;q=60",
+        image2:"https://apollo-singapore.akamaized.net/v1/files/r4q8xdw806x81-IN/image;s=780x0;q=60",
+        image3:"https://apollo-singapore.akamaized.net/v1/files/5r0sbn0rhlpq2-IN/image;s=780x0;q=60",
+        price:"4,45,000",
+        address:"GKNM Hospital, Coimbatore",
+        State:"Tamilnadu",
+        Description:"Color Grey Engine Capacity/Displacement (in Cc)1000 Insurance Type Comprehensive Make Month May Registration Place TN"
+    },
+    {
+        name:"iphone 13 PRO",
+        image1:"https://apollo-singapore.akamaized.net/v1/files/lb7xkx9hf3883-IN/image;s=780x0;q=60",
+        image2:"https://apollo-singapore.akamaized.net/v1/files/2gxzvlv4jawh2-IN/image;s=780x0;q=60",
+        image3:"https://apollo-singapore.akamaized.net/v1/files/m944frwg9exb2-IN/image;s=780x0;q=60",
+        price:"49,000",
+        address:"Samudrapur,Nagpur",
+        State:"Maharashtra",
+        Description:"15.49 cm (6.1 inch) Super Retina XDR Display 12MP + 12MP + 12MP | 12MP Front Camera A15 Bionic Chip Processor"
+    },
+    {
+        name:"Mahindra Kuv 100",
+        image1:"https://apollo-singapore.akamaized.net/v1/files/yfmgytmr9vlt1-IN/image;s=943x0;q=60",
+        image2:"https://apollo-singapore.akamaized.net/v1/files/f03evtrdnkj-IN/image;s=780x0;q=60",
+        image3:"https://apollo-singapore.akamaized.net/v1/files/u97k7zkmxun02-IN/image;s=640x0;q=60",
+        price:"3,35,000",
+        address:"Karkarduma, Delhi",
+        State:"Delhi",
+        Description:"KUV100 K6 PLUS, DIESEL 1ST OWNER, INSURED - MAY 2023 BOTH KEYS ADDITIONAL VEHICLE INFORMATION- Accidental- No Adjustable Steering- Yes Air Conditioning- With Heater Bluetooth- Yes Color- White Insurance Type: Third Party"
+    },
+    {
+        name:"Mahindra Kuv 100",
+        image1:"https://apollo-singapore.akamaized.net/v1/files/yfmgytmr9vlt1-IN/image;s=943x0;q=60",
+        image2:"https://apollo-singapore.akamaized.net/v1/files/f03evtrdnkj-IN/image;s=780x0;q=60",
+        image3:"https://apollo-singapore.akamaized.net/v1/files/u97k7zkmxun02-IN/image;s=640x0;q=60",
+        price:"3,35,000",
+        address:"Karkarduma, Delhi",
+        State:"Delhi",
+        Description:"KUV100 K6 PLUS, DIESEL 1ST OWNER, INSURED - MAY 2023 BOTH KEYS ADDITIONAL VEHICLE INFORMATION- Accidental- No Adjustable Steering- Yes Air Conditioning- With Heater Bluetooth- Yes Color- White Insurance Type: Third Party"
+    },
+    {
+        name:"ROYAL ENFIELD CONTINENTAL GT 650",
+        image1:"https://apollo-singapore.akamaized.net/v1/files/9i24qmlhdsl12-IN/image;s=780x0;q=60",
+        image2:"https://apollo-singapore.akamaized.net/v1/files/yvblrqkpfdee1-IN/image;s=780x0;q=60",
+        image3:"https://apollo-singapore.akamaized.net/v1/files/g6m7e524y2b51-IN/image;s=780x0;q=60",
+        price:"3,15,000",
+        address:"THE BIKE FACTORY SHOP NUMBER 38 39 VISHAL CHS SILVER PARK NEAR FIREBRIGADE AND SHIVSENA OFFICE MIRA RD EAST THANE 401107",
+        State:"Maharashtra",
+        Description:"ROYAL ENFIELD CONTINENTAL GT 650 JUST LIKE NEW BRAND NEW CONDITION ONLY 8 MONTHS OLD 3000km 1st owner 2KEYS"
+    },
+    {
+        name:"Xiomi 12 pro",
+        image1:"https://apollo-singapore.akamaized.net/v1/files/gl7m97t8rdbz1-IN/image;s=780x0;q=60",
+        image2:"https://apollo-singapore.akamaized.net/v1/files/8bgcr7b0d11z2-IN/image;s=780x0;q=60",
+        image3:"https://apollo-singapore.akamaized.net/v1/files/8u5wqfobgykw1-IN/image;s=780x0;q=60",
+        price:"55,000",
+        address:"Shree Gajanan Maharaj Nagar, Ulhasnagar, Maharashtra",
+        State:"Maharashtra",
+        Description:"Selling my brand new xiomi 12 pro 12 GB ram 256 GB memory 50 megapixel triple camera 8k recording Only use 1 month & 10 day's Condition- very good All box accessories available"
+    },
+    {
+        name:"Oppo F19s",
+        image1:"https://apollo-singapore.akamaized.net/v1/files/5tu4ihvw2dps1-IN/image;s=780x0;q=60",
+        image2:"https://apollo-singapore.akamaized.net/v1/files/fpk58p3zhzhy2-IN/image;s=780x0;q=60",
+        image3:"https://apollo-singapore.akamaized.net/v1/files/zvjyybz4nz6z1-IN/image;s=780x0;q=60",
+        price:"12,999",
+        address:"Ansari Nagar, Madurai, Tamil Nadu",
+        State:"Tamil Nadu",
+        Description:"Oppo F-19s,Used for 6 months, 1year inssurance available"
+    },
+    {
+        name:"Hero Mastro",
+        image1:"https://apollo-singapore.akamaized.net/v1/files/68olihem923s1-IN/image;s=780x0;q=60",
+        image2:"https://apollo-singapore.akamaized.net/v1/files/glhbleig28s23-IN/image;s=780x0;q=60",
+        image3:"https://apollo-singapore.akamaized.net/v1/files/d433ujow90bi-IN/image;s=780x0;q=60",
+        price:"28,000",
+        address:"Saat Rasta Road, Elephanta Caves, Maharashtra",
+        State:"Maharashtra",
+        Description:"Hero Maestro 2012 Done 23000 Kms in Awesome Condition in Original Paint Single Owner Meter not working ready to use all papers clear Genuine buyers contact no chitchat for more information call"
+    },
+    {
+        name:"Honda Dio",
+        image1:"https://apollo-singapore.akamaized.net/v1/files/s50awjirh45z1-IN/image;s=780x0;q=60 ",
+        image2:"https://apollo-singapore.akamaized.net/v1/files/lpdvu7ysc7qd3-IN/image;s=780x0;q=60",
+        image3:"https://apollo-singapore.akamaized.net/v1/files/0ikw83q28etr3-IN/image;s=780x0;q=60",
+        price:"64,000",
+        address:"Nellikkunnu, Kasaragod, Kerala",
+        State:"Kerla",
+        Description:"MAHINDRA BEST CHOICE Band road kasaragod *loan facility yes *We give one year Engine warranty *4 free service *63 points checked by First choice *Insurance : 2024"
+    },
+    {
+        name:"Mercedes-Benz Gla Class",
+        image1:"https://apollo-singapore.akamaized.net/v1/files/dg2d9vswojw-IN/image;s=780x0;q=60 ",
+        image2:"https://apollo-singapore.akamaized.net/v1/files/hlv0dm1jkvrh3-IN/image;s=780x0;q=60",
+        image3:"https://apollo-singapore.akamaized.net/v1/files/osms41fbe2ce3-IN/image;s=780x0;q=60",
+        price:"17,75,000",
+        address:"Bhikaji Cama Place, Delhi",
+        State:"Delhi",
+        Description:"Color: Silver Insurance Type: No Insurance Make Month: August Registration Place: DL"
+    },
+    {
+        name:"Mini Cooper Countryman ",
+        image1:"https://apollo-singapore.akamaized.net/v1/files/83w7zf4309r82-IN/image;s=780x0;q=60",
+        image2:"https://apollo-singapore.akamaized.net/v1/files/dtitme8hmdzc3-IN/image;s=780x0;q=60",
+        image3:"https://apollo-singapore.akamaized.net/v1/files/1sz4iuhf2hnz-IN/image;s=780x0;q=60",
+        price:"43,00,000",
+        address:"Kannur, Payyannur",
+        State:"Kerla",
+        Description:"Color: Red Engine Capacity/Displacement (in Cc): 1995 Insurance Type: Comprehensive Make Month: September Registration Place: KL"
+    },
+
+
+];
