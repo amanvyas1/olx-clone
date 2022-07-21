b4b4c038eb9c7476455d319d5c80df15f4664efe
abhishek11125 <103635309+abhishek11125@users.noreply.github.com>
2022-07-21 17:32:50 +0530

    Product details added

--- scripts/index.js
+++ scripts/index.js
@@ -109,6 +109,241 @@ let products = [
         State:"Kerla",
         Description:"Color: Red Engine Capacity/Displacement (in Cc): 1995 Insurance Type: Comprehensive Make Month: September Registration Place: KL"
     },
-
+    {
+        name: "Samsung Galaxy Note 8 ",
+        image1:
+          "https://apollo-singapore.akamaized.net/v1/files/lsdq3anwtywe1-IN/image;s=780x0;q=60",
+        image2:
+          "https://apollo-singapore.akamaized.net/v1/files/3w4fs7kmsags3-IN/image;s=780x0;q=60",
+        image3:
+          "https://apollo-singapore.akamaized.net/v1/files/lsdq3anwtywe1-IN/image;s=780x0;q=60",
+        price: "19,000",
+        address: "Bhai Randhir Singh Nagar,Ludhiana",
+        State: "Punjab",
+        Description:
+          "Black color. Immaculate condition. 6/64 box charger cable 100% clean. 100% laminated",
+      },
+      {
+        name: "iphone 12",
+        image1:
+          "https://apollo-singapore.akamaized.net/v1/files/lrf6i3nyy3b1-IN/image;s=780x0;q=60",
+        image2:
+          "https://apollo-singapore.akamaized.net/v1/files/b115n9eujug83-IN/image;s=780x0;q=60",
+        image3:
+          "https://apollo-singapore.akamaized.net/v1/files/zp4an3i2onou2-IN/image;s=780x0;q=60",
+        price: "30,000",
+        address: "Samudrapur MIDC, Maharashtra, India",
+        State: "Maharashtra",
+        Description:
+          "15.49 cm (6.1 inch) Super Retina XDR Display 12MP + 12MP | 12MP Front Camera A14 Bionic Chip with Next Generation Neural Engine Processor Ceramic Shield Industry-leading IP68 Water Resistance All Screen OLED Display 12MP TrueDepth Front Camera with Night Mode, 4K Dolby Vision HDR Recording",
+      },
+      {
+        name: "Realme buds q2",
+        image1:
+          "https://apollo-singapore.akamaized.net/v1/files/qd1i9sbg3u8t-IN/image;s=780x0;q=60",
+        image2:
+          "https://apollo-singapore.akamaized.net/v1/files/egiald3nlsj7-IN/image;s=780x0;q=60",
+        image3:
+          "https://apollo-singapore.akamaized.net/v1/files/4smqmuxqy6lv-IN/image;s=780x0;q=60",
+        price: "1700",
+        address: "Samudrapur MIDC, Maharashtra, India",
+        State: "Maharashtra",
+        Description: "Realme buds with high sound quality",
+      },
+      {
+        name: "Toyota Land Cruiser",
+        image1:
+          "https://apollo-singapore.akamaized.net/v1/files/7tjlyimvoi172-IN/image;s=780x0;q=60",
+        image2:
+          "https://apollo-singapore.akamaized.net/v1/files/d8fqjfi8q8lq2-IN/image;s=780x0;q=60",
+        image3:
+          "https://apollo-singapore.akamaized.net/v1/files/x45ilzf0npk7-IN/image;s=780x0;q=60",
+        price: "1,35,00,000",
+        address: "MG Road, Gurgaon",
+        State: "Haryana",
+        Description:
+          "Insurance Type: Comprehensive Registration Place: HR Exchange: Yes Finance: Yes",
+      },
+      {
+        name: "Land Rover Discovery Sport",
+        image1:
+          "https://apollo-singapore.akamaized.net/v1/files/2ltd4djfkblc2-IN/image;s=780x0;q=60",
+        image2:
+          "https://apollo-singapore.akamaized.net/v1/files/53a52lb49pbc-IN/image;s=780x0;q=60",
+        image3:
+          "https://apollo-singapore.akamaized.net/v1/files/ffbg7tiru93w2-IN/image;s=780x0;q=60",
+        price: "43,00,000",
+        address: "MG Road, Gurgaon",
+        State: "Haryana",
+        Description:
+          "*LAND LOVER DISCOVERY SPORT HSE* *SEVEN SEATER**2018* REGISTRATION*2017* MANUFACTURING*WHITE* WITH *BEIGE* INTERIOR ONLY *44000KM* WITH SERVICE RECORD*HR* NUMBER*FIRST* OWNER",
+      },
+      {
+        name: "Quick Shel Hand Gripper ",
+        image1:
+          "https://apollo-singapore.akamaized.net/v1/files/tdrvblqotpaq3-IN/image;s=780x0;q=60",
+        image2:
+          "https://apollo-singapore.akamaized.net/v1/files/vss8h3sc8ais1-IN/image;s=780x0;q=60",
+        image3:
+          "https://apollo-singapore.akamaized.net/v1/files/wyowr60ol23m1-IN/image;s=780x0;q=60",
+        price: "299",
+        address: "Samudrapur, Maharashtra, India",
+        State: "Maharashtra",
+        Description:
+          "Quick Shel Hand Gripper For Best Hand Exerciser Grip Adjustable 10kg to 40kg Hand Grip/Fitness Grip",
+      },
+      {
+        name: "2BHK Flat in Rajendra Park with Car Parking & CCTV ",
+        image1:
+          "https://apollo-singapore.akamaized.net/v1/files/ip0zvjgo50ei2-IN/image;s=780x0;q=60",
+        image2:
+          "https://apollo-singapore.akamaized.net/v1/files/9in6oj6kptuq2-IN/image;s=780x0;q=60",
+        image3:
+          "https://apollo-singapore.akamaized.net/v1/files/d6hiruq3v9qj2-IN/image;s=780x0;q=60",
+        price: "26,00,011",
+        address: "Rajendra Park,Gurgaon",
+        State: "Haryana",
+        Description:
+          "2BHK Flat in Rajendra Park with Ground Floor Car Parking with Pakki Registry and Home Loan with Subsidy Scheme, it's Low Budget Housing under Housing for all Scheme",
+      },
+      {
+        name: "Trendy analogous watches",
+        image1:
+          "https://apollo-singapore.akamaized.net/v1/files/f5leatovswri1-IN/image;s=780x0;q=60",
+        image2:
+          "https://apollo-singapore.akamaized.net/v1/files/me1r2p5jodfy1-IN/image;s=780x0;q=60",
+        image3:
+          "https://apollo-singapore.akamaized.net/v1/files/hpsqqqvncc15-IN/image;s=780x0;q=60",
+        price: "999",
+        address: "Samudrapur, Maharashtra, India",
+        State: "Maharashtra",
+        Description: "Trendy analogous watches",
+      },
+      {
+        name: "Maruti Suzuki Swift Dzire ",
+        image1:
+          "https://apollo-singapore.akamaized.net/v1/files/402lqbs8vpuc3-IN/image;s=780x0;q=60",
+        image2:
+          "https://apollo-singapore.akamaized.net/v1/files/z0xl9qr3oads2-IN/image;s=780x0;q=60",
+        image3:
+          "https://apollo-singapore.akamaized.net/v1/files/a2hcrv6vod983-IN/image;s=780x0;q=60",
+        price: " 7,60,000",
+        address: "Tirumala Colony, Hyderabad",
+        State: "Telangana",
+        Description:
+          "Color: Silver Insurance Type: Zero Dep Make Month: February Registration Place: TS",
+      },
+      {
+        name: "APPLE IPHONE 11 ",
+        image1:
+          "https://apollo-singapore.akamaized.net/v1/files/xmim8nuhuj3s1-IN/image;s=780x0;q=60",
+        image2:
+          "https://apollo-singapore.akamaized.net/v1/files/y0ems3gn78tq-IN/image;s=780x0;q=60",
+        image3:
+          "https://apollo-singapore.akamaized.net/v1/files/v96403tzv9sf-IN/image;s=780x0;q=60",
+        price: " 18,999",
+        address: "Samudrapur, Maharashtra, India",
+        State: "Maharashtra",
+        Description:
+          "15.49 cm (6.1 inch) Liquid Retina HD Display 12MP + 12MP | 12MP Front Camera A13 Bionic Chip Processor COD Availavble* Hurry up limited stock available* Brand 6 months warranty",
+      },
+      {
+        name: "MARUTI SUZUKI SUPER CARRY CNG",
+        image1:
+          "https://apollo-singapore.akamaized.net/v1/files/p929voj3w5xr1-IN/image;s=780x0;q=60",
+        image2:
+          "https://apollo-singapore.akamaized.net/v1/files/ypbz3rb235kg-IN/image;s=780x0;q=60",
+        image3:
+          "https://apollo-singapore.akamaized.net/v1/files/zqpkla7fe3pg2-IN/image;s=780x0;q=60",
+        price: " 5,40,000",
+        address: "New Moti Nagar, Delhi",
+        State: "Delhi",
+        Description:
+          "MARUTI SUZUKI SUPER CARRY*CNG*CLOSED BODY*DELHI REG.*PETROL AND CNG HYBRID",
+      },
+      {
+        name: "EICHER 1110",
+        image1:
+          "https://apollo-singapore.akamaized.net/v1/files/p9euwrv8wcw4-IN/image;s=780x0;q=60",
+        image2:
+          "https://apollo-singapore.akamaized.net/v1/files/ftcetbtuyonm1-IN/image;s=780x0;q=60",
+        image3:
+          "https://apollo-singapore.akamaized.net/v1/files/c7a79tq8fdet3-IN/image;s=780x0;q=60",
+        price: "6,90,000",
+        address: "Kidangoor, Angamaly, Kerala",
+        State: "Kerla",
+        Description: "Eicher 1110 2012 model 5 new tyrs New test New isurance",
+      },
+      {
+        name: "Mahindra Thar",
+        image1:
+          "https://apollo-singapore.akamaized.net/v1/files/u7glbr5xjvqb1-IN/image;s=780x0;q=60",
+        image2:
+          "https://apollo-singapore.akamaized.net/v1/files/xvzmsrhjuooq1-IN/image;s=780x0;q=60",
+        image3:
+          "https://apollo-singapore.akamaized.net/v1/files/hjzdns9hl7y4-IN/image;s=780x0;q=60",
+        price: "4,15,000",
+        address: "Nagali Village",
+        State: "Himachal Pradesh",
+        Description:
+          "All India transport dilAvri power Stearing power break power clutch All documents kamplit",
+      },
+      {
+        name: "125CC NEO ATV",
+        image1:
+          "https://apollo-singapore.akamaized.net/v1/files/bcd0xhgs58yz2-IN/image;s=780x0;q=60",
+        image2:
+          "https://apollo-singapore.akamaized.net/v1/files/q5794oh9498q1-IN/image;s=780x0;q=60",
+        image3:
+          "https://apollo-singapore.akamaized.net/v1/files/f3jp8kou324y1-IN/image;s=780x0;q=60",
+        price: "74,999",
+        address: "Nanded, Nanded, Maharashtra",
+        State: "Maharashtra",
+        Description:
+          "125CC FOUR STROKE ENGINE,SEMI AUTOMATIC TRANSMISSION(F-N-R),AVAILABLE IN DIFFERENT COLOURS.",
+      },
+      {
+        name: "SUZUKI GIXXER SF",
+        image1:
+          "https://apollo-singapore.akamaized.net/v1/files/n7yw9to9sph22-IN/image;s=780x0;q=60",
+        image2:
+          "https://apollo-singapore.akamaized.net/v1/files/pemqmay7q3sl2-IN/image;s=780x0;q=60",
+        image3:
+          "https://apollo-singapore.akamaized.net/v1/files/wlcsh2r4tfab2-IN/image;s=780x0;q=60",
+        price: "1,30,000",
+        address: "Param Jeet Ganj, Kapurthala, Punjab",
+        State: "Punjab",
+        Description: "Brand: Suzuki,Model:Gixxer,Year:2019, KM driven: 2,000 km",
+      },    
 
 ];
+
+
+let container = document.getElementById('products');
+container.innerHTML = null
+
+let append=(products)=>{
+    products.forEach((el) => {
+        let box = document.createElement('div');
+
+        let pic = document.createElement('img');
+        pic.src = el.image1;
+
+        let price = document.createElement('h3');
+        price.innerText = "₹"+el.price;
+
+        let name = document.createElement('p')
+        name.innerText = el.name;
+
+        let address = document.createElement('p')
+        address.innerText = el.address;
+
+        box.append(pic, price, name, address);
+        container.append(box)
+    });
+}
+
+append(products);
+
+
+
