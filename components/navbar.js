df716730fcd2063c8c00fc61043bd79fced5d0f5
gitesh8 <giteshsutar88@gmail.com>
2022-07-21 21:39:42 +0530

    files added

--- components/navbar.js
+++ components/navbar.js
@@ -1,31 +1,45 @@
-let desktop_navbar=()=>{
+let navbar = () =>{
     return `
-    <div id="header">
-    <div><img src="https://upload.wikimedia.org/wikipedia/commons/9/91/Logotyp_OLX_.png" alt=""></div>
-    <div><select>
-        <option >Maharashtra</option>
-        <option >Maharashtra</option>
-        <option >Maharashtra</option>
-        <option >Maharashtra</option>
-        <option >Maharashtra</option>
-    </select></div>
-    <div>
-        <input type="text" placeholder="Find Cars, Mobile Phones and More...">
-        <button><i class="fa fa-search"></i></button>
+    <div id="header-container">
+        <div id="header">
+            <div><img src="https://upload.wikimedia.org/wikipedia/commons/9/91/Logotyp_OLX_.png" alt=""></div>
+            <div><select>
+                <option >Maharashtra</option>
+                <option >Maharashtra</option>
+                <option >Maharashtra</option>
+                <option >Maharashtra</option>
+                <option >Maharashtra</option>
+            </select></div>
+            <div>
+                <input type="text" placeholder="Find Cars, Mobile Phones and More...">
+                <button><i class="fa fa-search"></i></button>
+            </div>
+                
+            <div><select >
+                <option >ENGLISH </option>
+            </select></div>
+            <div><i class="fa-regular fa-comment"></i></div>
+            <div><i class="fa-regular fa-bell"></i></div>
+            <div><i class="fa-regular fa-circle"></i></div>
+            <div><button>SELL</button></div>
+        </div>
     </div>
+    <div id="divider">
         
-    <div><select >
-        <option >ENGLISH </option>
-    </select></div>
-    <div><i class="fa-regular fa-comment"></i></div>
-    <div><i class="fa-regular fa-bell"></i></div>
-    <div><i class="fa-regular fa-circle"></i></div>
-    <div><button>SELL</button></div>
-</div>`
-}
-let mobile_navbar=()=>{
-    return `
-    <div id="head">
+    </div>
+    <div id="menu">
+        <h4>ALL CATEGORIES</h4>
+        <a href="">Cars</a>
+        <a href="">Motorcycles</a>
+        <a href="">Mobile Phones</a>
+        <a href="">For Sale: Houses & Apartments</a>
+        <a href="">Scooters</a>
+        <a href="">Commercial & Other Vehicles</a>
+        <a href="">For Rent: Houses & Apartments</a>
+    </div>
+
+    <div id="mobile-header">
+        <div id="head">
             <div>
                <div> <i class="fa-solid fa-bars"></i></div>
                 <div><img src="https://upload.wikimedia.org/wikipedia/commons/9/91/Logotyp_OLX_.png" ></div>
@@ -35,21 +49,10 @@ let mobile_navbar=()=>{
         <div id="search">
             <input type="text"  placeholder="Find Cars, Mobile Phones and More...">
             <i class="fa fa-search"></i>
-    </div>`
+    </div>
+        
+        
+    </div>
+    `
 }
-
-let menu = () =>{
-    return `
-    <h4>ALL CATEGORIES</h4>
-    <a href="">Cars</a>
-    <a href="">Motorcycles</a>
-    <a href="">Mobile Phones</a>
-    <a href="">For Sale: Houses & Apartments</a>
-    <a href="">Scooters</a>
-    <a href="">Commercial & Other Vehicles</a>
-    <a href="">For Rent: Houses & Apartments</a>`
-}
-
-
-export  {desktop_navbar,mobile_navbar,menu};
-
+export default navbar;
