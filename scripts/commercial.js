46ee77e031f9d2b7ff4b18e999e6fce11935334b
Nikhil Patil <nikhildpatil976@gmail.com>
2022-07-23 01:12:34 +0530

    adding functionality

--- scripts/commercial.js
+++ scripts/commercial.js
@@ -1,21 +1,21 @@
    let products_details=[
    { name:"Hyundai Xcent Base",
     image:"https://apollo-singapore.akamaized.net/v1/files/p6x4f64p9thr2-IN/image;s=300x600;q=60",
-    price: "₹ 5,20,000",
+    price: 520000,
     adress:"Shyam Bazar",
     state:"Kolkata"
   },
   {
     name:"Hyundai Xcent Base",
     image:"https://apollo-singapore.akamaized.net/v1/files/ccz8pf0ie2q23-IN/image;s=300x600;q=60",
-    price: "₹ 1,00,000",
+    price: 100000,
     adress:"miya Bazar",
     state:"Kolkata"
   },
   {
     name:"Maruti Suzuki Wagnor H3",
     image:"https://apollo-singapore.akamaized.net/v1/files/w2d1dwk87fqb3-IN/image;s=300x600;q=60",
-    price: "₹ 40,000",
+    price:  40000,
     adress:"Mumbai",
     state:"Maharashtra"
 
@@ -24,7 +24,7 @@
   {
     name:"Bolero",
     image:"https://apollo-singapore.akamaized.net/v1/files/zwnz8567xo2x-IN/image;s=300x600;q=60",
-    price: "₹ 2,78,000",
+    price:  278000,
     adress:"Thane,Mumbai",
     state:"Maharashtra"
 
@@ -32,7 +32,7 @@
   {
     name:"B.S. Agro Rotavators",
     image:"https://apollo-singapore.akamaized.net/v1/files/sc27g3glbec22-IN/image;s=300x600;q=60",
-    price: "₹ 80,000",
+    price: 80000,
     adress:"Bhopal",
     state:"Madhya Pradesh"
 
@@ -40,7 +40,7 @@
   {
     name:"Willy Jeep",
     image:"https://apollo-singapore.akamaized.net/v1/files/r2hh5gvd85l93-IN/image;s=300x600;q=60",
-    price: "₹ 4,43,000",
+    price:  443000,
     adress:"Indore",
     state:"Madhya Pradesh"
 
@@ -48,7 +48,7 @@
   {
     name:"Piaggio-Appe",
     image:"https://apollo-singapore.akamaized.net/v1/files/ezwjfbefdsd32-IN/image;s=300x600;q=60",
-    price: "₹ 33,000",
+    price: 33000,
     adress:"Pkase1 Mohali",
     state:"Panjab"
 
@@ -56,7 +56,7 @@
   {
     name:"Vintage wedding replica car",
     image:"https://apollo-singapore.akamaized.net/v1/files/c0ymdqcavxfu-IN/image;s=300x600;q=60",
-    price: "₹ 5,53,000",
+    price:  553000,
     adress:"Amritsar",
     state:"Panjab"
 
@@ -64,28 +64,28 @@
   {
     name:" Restored Vintage car",
     image:"https://apollo-singapore.akamaized.net/v1/files/nxgtwxxqgj783-IN/image;s=300x600;q=60",
-    price: "₹ 6,33,000",
+    price:  633000,
     adress:"Jaipur",
     state:"Rajsthan"
 },
 {
 name:"Jeep",
 image:"https://apollo-singapore.akamaized.net/v1/files/fczdverzowpi3-IN/image;s=300x600;q=60",
-price: "₹ 3,49,000",
+price:  349000,
 adress:"Rajkot",
 state:"Rajsthan"
 },
 {
 name:"Mahindra Bolero Maxi Truck",
 image:"https://apollo-singapore.akamaized.net/v1/files/703w3f2pm9db1-IN/image;s=300x600;q=60",
-price: "₹ 6,62,000",
+price: 662000,
 adress:"Ayodhya",
 state:"Uttar Pradesh"
 },
 {
 name:"Eicher School Bus",
 image:"https://apollo-singapore.akamaized.net/v1/files/049619w7zzf7-IN/image;s=300x600;q=60",
-price: "₹ 9,60,000",
+price:  960000,
 adress:"Varanasi",
 state:"Uttar Pradesh"
 },
@@ -93,58 +93,61 @@ state:"Uttar Pradesh"
 
 ]
 
-let more_data=[
+let moreData=[
     {
         name:" Restored Vintage car",
         image:"https://apollo-singapore.akamaized.net/v1/files/nxgtwxxqgj783-IN/image;s=300x600;q=60",
-        price: "₹ 6,33,000",
+        price:  633000,
         adress:"Jaipur",
         state:"Rajsthan"
     },
     {
     name:"Jeep",
     image:"https://apollo-singapore.akamaized.net/v1/files/fczdverzowpi3-IN/image;s=300x600;q=60",
-    price: "₹ 3,49,000",
+    price: 349000,
     adress:"Rajkot",
     state:"Rajsthan"
     },
     {
     name:"Mahindra Bolero Maxi Truck",
     image:"https://apollo-singapore.akamaized.net/v1/files/703w3f2pm9db1-IN/image;s=300x600;q=60",
-    price: "₹ 6,62,000",
+    price:  662000,
     adress:"Ayodhya",
     state:"Uttar Pradesh"
     },
     {
     name:"Eicher,School Bus",
     image:"https://apollo-singapore.akamaized.net/v1/files/049619w7zzf7-IN/image;s=300x600;q=60",
-    price: "₹ 9,60,000",
+    price: 960000,
     adress:"Varanasi",
     state:"Uttar Pradesh"
     },
     { name:"Hyundai Xcent Base",
     image:"https://apollo-singapore.akamaized.net/v1/files/p6x4f64p9thr2-IN/image;s=300x600;q=60",
-    price: "₹ 5,20,000",
+    price: 520000,
     adress:"Shyam Bazar",
     state:"Kolkata"
   },
   {
     name:"Hyundai Xcent Base",
     image:"https://apollo-singapore.akamaized.net/v1/files/ccz8pf0ie2q23-IN/image;s=300x600;q=60",
-    price: "₹ 1,00,000",
+    price: 100000,
     adress:"miya Bazar",
     state:"Kolkata"
   }
 
 ]
-// console.log(product_details)
+
 localStorage.setItem('products_details',JSON.stringify(products_details));
 
 let data = JSON.parse(localStorage.getItem('products_details'));
 
 let container=document.getElementById("commercial")
 
-data.forEach(function(el){
+
+function append(data){
+  container.innerHTML=null
+  data.forEach(function(el){
     let div=document.createElement("div")
 
     let name=document.createElement("h3")
@@ -161,28 +164,53 @@ data.forEach(function(el){
     container.append(div)
 })
 
-// let btn = document.getElementById('btn-load')
-// more_data.forEach(function(el){
-//     let div=document.createElement("div")
+}
+append(data)
+function append2(moreData){
+    
+moreData.forEach(function(el){
+    let div=document.createElement("div")
 
-//     let name=document.createElement("h2")
-//     name.innerText=el.name
-//     let image=document.createElement("img")
-//     image.src=el.image
-//     let price=document.createElement("h2")
-//     price.innerText=el.price
-//     let adress=document.createElement("h5")
-//     adress.innerText=el.adress
-//     let state=document.createElement("h5")
-//     state.innerText=el.state
-//     div.append(name,image,price,adress,state)
-//     btn.append(div)
-// })
+    let name=document.createElement("h2")
+    name.innerText=el.name
+    let image=document.createElement("img")
+    image.src=el.image
+    let price=document.createElement("h2")
+    price.innerText=el.price
+    let adress=document.createElement("h5")
+    adress.innerText=el.adress
+    let state=document.createElement("h5")
+    state.innerText=el.state
+    div.append(name,image,price,adress,state)
+    container.append(div)
+
+  
+})
+
+}
 
 
-// btn.addEventListener('click',addData)
+let btn1= document.getElementById('btn-load');
+
+btn1.addEventListener('click',addData)
 
 
-// function addData(){
-//   append(more_data);
-// }
+function addData(){
+  append2(moreData);
+}
+
+
+let btn2=document.getElementById("btnback");
+window.onscroll=function(){
+  scrollFunction()
+}
+function  scrollFunction(){
+  if(document.body.scrollTop>500 || document.documentElement.scrollTop>500){
+    btn2.style.display='block';
+  }else{
+    btn2.style.display='none';
+  }
+}
+function backTop(){
+  document.documentElement.scrollTop=0;
+}
--- scripts/scooter.js
+++ scripts/scooter.js
@@ -2,25 +2,25 @@
 
 
 
-let product_details=[
+let products_details=[
     {
         name:"Honda dio",
         image:"https://apollo-singapore.akamaized.net/v1/files/2e5cvsaawmly-IN/image;s=300x600;q=60",
-        price: "₹ 35,000",
+        price: 35000,
         adress:"Shyam Bazar",
         state:"Kolkata"
       },
       {
         name:"Honda km driven",
         image:"https://apollo-singapore.akamaized.net/v1/files/h9xvd6puu6f03-IN/image;s=300x600;q=60",
-        price: "₹ 22,000",
+        price: 22000,
         adress:"miya Bazar",
         state:"Kolkata"
       },
       {
         name:"TVS Jupitor",
         image:"https://apollo-singapore.akamaized.net/v1/files/fjcyr90js5yv-IN/image;s=300x600;q=60",
-        price: "₹ 40,000",
+        price: 40000,
         adress:"Mumbai",
         state:"Maharashtra"
 
@@ -29,7 +29,7 @@ let product_details=[
       {
         name:"Honda Activa Bs4",
         image:"https://apollo-singapore.akamaized.net/v1/files/87wrbewqrprh1-IN/image;s=300x600;q=60",
-        price: "₹ 50,000",
+        price: 50000,
         adress:"Thane,Mumbai",
         state:"Maharashtra"
 
@@ -37,7 +37,7 @@ let product_details=[
       {
         name:"TVS Jupitor",
         image:"https://apollo-singapore.akamaized.net/v1/files/prjt1qbhnyfn-IN/image;s=300x600;q=60",
-        price: "₹ 46,000",
+        price: 46000,
         adress:"Bhopal",
         state:"Madhya Pradesh"
 
@@ -45,7 +45,7 @@ let product_details=[
       {
         name:"Hero Pleasure Plus",
         image:"https://apollo-singapore.akamaized.net/v1/files/q93sbt354ee53-IN/image;s=300x600;q=60",
-        price: "₹ 43,000",
+        price: 43000,
         adress:"Indore",
         state:"Madhya Pradesh"
 
@@ -53,7 +53,7 @@ let product_details=[
       {
         name:"Suzuki burgman",
         image:"https://apollo-singapore.akamaized.net/v1/files/tolqhtcbzi5i-IN/image;s=300x600;q=60",
-        price: "₹ 53,000",
+        price: 53000,
         adress:"Pkase1 Mohali",
         state:"Panjab"
 
@@ -61,7 +61,7 @@ let product_details=[
       {
         name:"Honda Activa",
         image:"https://apollo-singapore.akamaized.net/v1/files/dl4eqr05pomx1-IN/image;s=300x600;q=60",
-        price: "₹ 53,000",
+        price: 53000,
         adress:"Amritsar",
         state:"Panjab"
 
@@ -69,28 +69,28 @@ let product_details=[
       {
         name:"Honda Activa",
         image:"https://apollo-singapore.akamaized.net/v1/files/4i43xuhkrmek1-IN/image;s=300x600;q=60",
-        price: "₹ 33,000",
+        price: 33000,
         adress:"Jaipur",
         state:"Rajsthan"
  },
  {
     name:"Honda Pleasure",
     image:"https://apollo-singapore.akamaized.net/v1/files/8gmxljcozsw83-IN/image;s=300x600;q=60",
-    price: "₹ 49,000",
+    price: 49000,
     adress:"Rajkot",
     state:"Rajsthan"
 },
 {
     name:"Honda Activa",
     image:"https://apollo-singapore.akamaized.net/v1/files/ro9ta25j08fx2-IN/image;s=300x600;q=60",
-    price: "₹ 62,000",
+    price: 62000,
     adress:"Ayodhya",
     state:"Uttar Pradesh"
 },
 {
     name:"Tvs Jupitor",
     image:"https://apollo-singapore.akamaized.net/v1/files/uaj5ixtlng471-IN/image;s=300x600;q=60",
-    price: "₹ 60,000",
+    price: 60000,
     adress:"Varanasi",
     state:"Uttar Pradesh"
 },
@@ -98,12 +98,66 @@ let product_details=[
 
 ]
 
-// console.log(product_details)
+let moreData=[
+  {
+    name:"Suzuki burgman",
+    image:"https://apollo-singapore.akamaized.net/v1/files/tolqhtcbzi5i-IN/image;s=300x600;q=60",
+    price:  53000,
+    adress:"Pkase1 Mohali",
+    state:"Panjab"
+
+  },
+  {
+    name:"Honda Activa",
+    image:"https://apollo-singapore.akamaized.net/v1/files/dl4eqr05pomx1-IN/image;s=300x600;q=60",
+    price: 53000,
+    adress:"Amritsar",
+    state:"Panjab"
+
+  },
+  {
+    name:"Honda Activa",
+    image:"https://apollo-singapore.akamaized.net/v1/files/4i43xuhkrmek1-IN/image;s=300x600;q=60",
+    price: 33000,
+    adress:"Jaipur",
+    state:"Rajsthan"
+},
+{
+name:"Honda Pleasure",
+image:"https://apollo-singapore.akamaized.net/v1/files/8gmxljcozsw83-IN/image;s=300x600;q=60",
+price: 49000,
+adress:"Rajkot",
+state:"Rajsthan"
+},
+{
+name:"Honda Activa",
+image:"https://apollo-singapore.akamaized.net/v1/files/ro9ta25j08fx2-IN/image;s=300x600;q=60",
+price: 62000,
+adress:"Ayodhya",
+state:"Uttar Pradesh"
+},
+{
+name:"Tvs Jupitor",
+image:"https://apollo-singapore.akamaized.net/v1/files/uaj5ixtlng471-IN/image;s=300x600;q=60",
+price: 60000,
+adress:"Varanasi",
+state:"Uttar Pradesh"
+},
 
 
-let scooters=document.getElementById("scooters")
+]
 
-product_details.forEach(function(el){
+localStorage.setItem('products_details',JSON.stringify(products_details));
+
+let data = JSON.parse(localStorage.getItem('products_details'));
+
+
+let container=document.getElementById("scooters")
+
+
+function append(data){
+  container.innerHTML=null
+  products_details.forEach(function(el){
     let div=document.createElement("div")
 
     let name=document.createElement("h2")
@@ -118,7 +172,75 @@ product_details.forEach(function(el){
     let state=document.createElement("h4")
     state.innerText=el.state
     div.append(name,image,price,adress,state)
-    scooters.append(div)
+    container.append(div)
 })
+
+
+}
+append(data)
+
+function append2(moreData){
+    
+  moreData.forEach(function(el){
+      let div=document.createElement("div")
+  
+      let name=document.createElement("h2")
+      name.innerText=el.name
+      let image=document.createElement("img")
+      image.src=el.image
+      let price=document.createElement("h2")
+      price.innerText=el.price
+      let adress=document.createElement("h5")
+      adress.innerText=el.adress
+      let state=document.createElement("h5")
+      state.innerText=el.state
+      div.append(name,image,price,adress,state)
+      container.append(div)
+  
+    
+  })
+  
+  }
+  
+  
+  let btn1= document.getElementById('btn-load');
+  
+  btn1.addEventListener('click',addData)
+  
+  
+  function addData(){
+    append2(moreData);
+  }
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let btn2=document.getElementById("btnback");
+window.onscroll=function(){
+  scrollFunction()
+}
+function  scrollFunction(){
+  if(document.body.scrollTop>500 || document.documentElement.scrollTop>500){
+    btn2.style.display='block';
+  }else{
+    btn2.style.display='none';
+  }
+}
+function backTop(){
+  document.documentElement.scrollTop=0;
+}
 
 
